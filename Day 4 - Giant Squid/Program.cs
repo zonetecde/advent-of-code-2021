1271e8cb47cb839e3849a53c069fb35aec000852
zonetecde <zonetecde@gmail.com>
2022-09-12 20:29:52 +0200

    12/09/2022

---
+++ Day 4 - Giant Squid/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_4___Giant_Squid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://adventofcode.com/2021/day/4
+            List<string> inputs = File.ReadAllLines(@"..\..\input.txt").ToList();
+
+            // l1. bingo's number
+            List<int> bingosNumber = inputs[0].Split(',').Select(x => int.Parse(x)).ToList();
+
+            // permet de faire fonctionner l'algorithme de récup des boards
+            inputs = inputs.ConvertAll(x => { return x.Replace("  ", " "); }).ConvertAll(y =>
+            {
+                if (y.StartsWith(" "))
+                    return y.Remove(0, 1);
+                else return y;
+            });
+
+            List<int[,]> boards = new List<int[,]>();
+
+            for (int i = 2; i < inputs.Count; i += 6)
+            {
+                boards.Add(new int[5, 5]);
+                for (int y = 0; y < 5; y++)               
+                    for (int x = 0; x < 5; x++)                
+                        boards.Last()[y, x] = Convert.ToInt32(inputs[i + y].Split(' ')[x]);                                
+            }
+
+            List<int> boardWhoWon = new List<int>();
+
+            foreach (int number in bingosNumber)
+            {
+                boards.ForEach(board =>
+                {
+                    for (int y = 0; y < 5; y++)
+                        for (int x = 0; x < 5; x++)
+                            if (board[y, x] == number)
+                            {
+                                board[y, x] = -1;
+
+                                // si la colonne ou la ligne n'a que des -1 alors c'est bingo
+                                int minusOneInRow = 0;
+                                int minusOneInColumn = 0;
+                                for (int i = 0; i < 5; i++)
+                                {
+                                    if(board[i, x] == -1)
+                                    {
+                                        minusOneInColumn++;
+                                    }
+                                    if(board[y, i] == -1)
+                                    {
+                                        minusOneInRow++;
+                                    }
+                                }
+
+                                // bingo si == 5
+                                if(minusOneInRow == 5 || minusOneInColumn == 5)
+                                {
+                                    // somme de tout les nombres unmarked dans le tableau
+                                    int finalScore = 0;
+                                    for (int x2 = 0; x2 < 5; x2++)
+                                    {
+                                        for (int y2 = 0; y2 < 5; y2++)
+                                        {
+                                            if (board[x2, y2] != -1)
+                                                finalScore += board[x2, y2];
+                                        }
+                                    }
+
+                                    // multiplier par le dernier nombre
+                                    finalScore *= number;
+
+                                    // pour pas qu'un même tableau win 2 fois
+                                    if (boardWhoWon.Contains(boards.IndexOf(board)))
+                                        return;
+                                    
+                                    boardWhoWon.Add(boards.IndexOf(board));
+
+                                    if (nbOfWin == 1)
+                                        Console.WriteLine("part1 : " + finalScore);
+                                    if (boardWhoWon.Count == boards.Count)
+                                        Console.WriteLine("part2 : " + finalScore);                                   
+
+                                    return;
+                                }
+                            }
+                });
+            }
+        }
+    }
+}
---
+++ Day 5 - Hydrothermal Venture/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_5___Hydrothermal_Venture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://adventofcode.com/2021/day/5
+            List<string[]> inputs = File.ReadAllLines(@"..\..\input.txt").ToList().ConvertAll(x =>
+            {
+                return x.Split(new string[] { " -> " }, StringSplitOptions.None);           
+            });
+
+            // remove all non-vertical or non-horizontal lines
+            // x1 = x2 or y1 = y2 = vertical/horizontal
+            inputs.RemoveAll(x =>
+            {
+                if (x[0].Split(',')[0] == x[1].Split(',')[0]
+                ||
+                x[0].Split(',')[1] == x[1].Split(',')[1])
+                    return false;
+                else
+                    return true;
+            });
+
+            int[,] board = new int[1000, 1000];
+            for (int x = 0; x < 1000; x++)            
+                for (int y = 0; y < 1000; y++)                
+                    board[x, y] = 0;
+                
+            
+
+            inputs.ForEach(input =>
+            {
+                int x1 = Convert.ToInt32(input[0].Split(',')[0]);
+                int y1 = Convert.ToInt32(input[0].Split(',')[1]);
+
+                int x2 = Convert.ToInt32(input[1].Split(',')[0]);
+                int y2 = Convert.ToInt32(input[1].Split(',')[1]);
+
+                // trace les lignes
+                if(x1 == x2) // ligne verticales
+                {
+                    int firstY = y2 < y1 ? y2 : y1; // le plus petit des 2
+                    int secondY = y2 < y1 ? y1 : y2; // le plus grand des 2
+
+                    for (int i = firstY; i < secondY + 1; i++)
+                    {
+                        board[i, x1] += 1;
+                    }
+                }
+                else // ligne horizontal
+                {
+                    int firstX = x2 < x1 ? x2 : x1; // le plus petit des 2
+                    int secondX = x2 < x1 ? x1 : x2; // le plus grand des 2
+
+                    for (int i = firstX; i < secondX + 1; i++)
+                    {
+                        board[y1, i] += 1;
+                    }
+                }
+            });
+
+            int atLeast2lines = 0;
+
+            for (int x = 0; x < 1000; x++)
+                for (int y = 0; y < 1000; y++)
+                    if(board[x, y] >= 2)
+                        atLeast2lines++;
+
+            Console.WriteLine("part1 : " + atLeast2lines);
+        }
+    }
+}
