b39c91030d167f23e92c31b49a6223e89f7b421b
zonetecde <zonetecde@gmail.com>
2022-09-17 13:16:46 +0200

    17/09/2022 13:16

---
+++ Day 14 - Extended Polymerization/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_14___Extended_Polymerization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //https://adventofcode.com/2021/day/14
+
+            List<string> inputs = File.ReadAllLines(@"..\..\input.txt").ToList();
+
+            // xy -> z
+            List<string[]> criteres = new List<string[]>();
+
+            // phrase base
+            string output = inputs[0];
+
+            // i = 2 car les chaînes commences à ligne 3
+            // ajoute les critères
+            for (int i = 2; i < inputs.Count; i++)
+            {
+                criteres.Add(new string[2]
+                {
+                    inputs[i].Split(new string[] {" -> "}, StringSplitOptions.None)[0],
+                    inputs[i].Split(new string[] {" -> "}, StringSplitOptions.None)[1]
+                });
+            }
+
+            // t[0] = indexToAdd, t[1] = letterToAdd
+            List<string[]> indexes = new List<string[]>();
+            for (int step = 0; step < 40; step++)
+            {
+                // Applique les critères
+                foreach (string[] critere in criteres)
+                {
+                    // Ajoute dans indexes tous les endroits ou le caractère doit être ajouté
+                    for (int index = 0; ; index += 1)
+                    {
+                        index = output.IndexOf(critere[0], index);
+                        if (index != -1)
+                            indexes.Add(new string[] { (index + 1).ToString(), critere[1] });
+                        else
+                            break;
+                    }
+                }
+
+                // Ajoute les caractères au output
+                int decallage = 0;
+                indexes = indexes.OrderBy(x => Convert.ToInt32(x[0])).ToList();
+                foreach (string[] index in indexes)
+                {
+                    output = output.Insert(Convert.ToInt32(index[0]) + decallage, index[1]);
+                    decallage++;
+                }
+
+                indexes.Clear();
+
+                Console.WriteLine(step);
+            }
+
+            // quantity of the most common element and subtract the quantity of the least common element
+            var temp = output.ToCharArray().ToList();
+            temp = temp.OrderBy(x => temp.Count(y => y == x)).ToList();
+            ulong part1Result = (ulong)temp.Count(x => x == temp.Last()) - (ulong)temp.Count(x => x == temp.First());
+
+            Console.WriteLine("part1 : " + part1Result);
+        }
+    }
+}
---
+++ Day 14 - Extended Polymerization/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("Day 14 - Extended Polymerization")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Day 14 - Extended Polymerization")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("ac852aed-3ecf-4d47-a8f4-b78b9f29a94d")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire
+//      Numéro de build
+//      Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
+// en utilisant '*', comme indiqué ci-dessous :
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
